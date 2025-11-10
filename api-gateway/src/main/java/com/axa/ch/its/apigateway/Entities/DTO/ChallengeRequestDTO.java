39e99e69ce7490d75d1be022f2f7af2cfe28d299
silvanaregger <silvanaregger@swissonline.ch>
2025-11-10 08:34:09 +0100

    api gateway

---
+++ api-gateway/src/main/java/com/axa/ch/its/apigateway/Entities/DTO/ChallengeRequestDTO.java
@@ -0,0 +1,13 @@
+package com.axa.ch.its.apigateway.Entities.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChallengeRequestDTO {
+
+    private Long id;
+    private String question;
+
+}
---
+++ api-gateway/src/main/java/com/axa/ch/its/apigateway/Entities/DTO/ChallengeResponseDTO.java
@@ -0,0 +1,12 @@
+package com.axa.ch.its.apigateway.Entities.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChallengeResponseDTO {
+
+    private Long id;
+    private String answer;
+}
